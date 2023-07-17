02b3756bb3aed93477e2561c01b9b8cb43d4e909
mujugen <101939481+mujugen@users.noreply.github.com>
2023-07-17 21:11:21 +0800

    made qr code generator

---
+++ script.js
@@ -0,0 +1,19 @@
+function generateQR() {
+  console.log("click");
+  let textarea = document.getElementById("preview");
+  let value = textarea.value;
+  console.log(value);
+
+  var encodedText = encodeURIComponent(value);
+
+  // Create the API URL with the encoded text
+  var apiUrl =
+    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+    encodedText;
+
+  var img = document.createElement("img");
+  img.src = apiUrl;
+
+  var container = document.getElementById("qrcode-container");
+  container.appendChild(img);
+}
